bfafe29b077ee66bca509aa9d0bb2c7d014757e9
inderpreet59173 <inderpreet59173singh@gmail.com>
2021-12-16 19:06:42 -0500

    initial commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,9 @@
+// IIFE -- Immediately invoked func expression
+(function(){
+    function Start()
+    {
+        console.log("App Started...");
+    }
+    
+    window.addEventListener("load", Start);
+})();
